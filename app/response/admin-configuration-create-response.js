7c9e2838640be12f6fcd40e27aae3ccd1449fe6b
ripperofdeath <marcelino.rui@gmail.com>
2015-09-12 23:05:43 +0100

    responses

--- app/response/admin-configuration-create-response.js
+++ app/response/admin-configuration-create-response.js
@@ -1,7 +1,7 @@
 var BaseResponse = require('./base-response.js'),
 	util = require('util');
 
-function Response(request, error){
+function Response(request){
 	BaseResponse.apply(this, arguments);
 }
 util.inherits(Response, BaseResponse);
